fd42b77a1056ac45075a1a03d058d10e641d65ae
TimiDev19 <rotimioluwatimilehin19@gmail.com>
2025-09-14 20:26:10 +0100

    pwa

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,26 @@
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+//   /* config options here */
+// };
+
+// export default nextConfig;
+
+// next.config.ts
+
 import type { NextConfig } from "next";
 
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const withPWA = require("next-pwa")({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === "development",
+});
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
 };
 
-export default nextConfig;
+export default withPWA(nextConfig);
+
